e0ca42dbcb68075012be96e7c6286d3d902c4bfc
jackvid <jakovvidak2303@gmail.com>
2023-11-14 15:32:31 +0100

    Added CORS filter

--- src/main/java/com/demo/movies/MoviesApplication.java
+++ src/main/java/com/demo/movies/MoviesApplication.java
@@ -3,6 +3,9 @@ package com.demo.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
